5ff7e32a7b9a62809c9c08795688e004afc5591f
Tal Hershkovich <dancingshapes@gmail.com>
2017-04-13 18:01:57 +0200

    Fixed!

--- bone_loc_link.py
+++ bone_loc_link.py
@@ -1,5 +1,15 @@
 import bpy
 
+def duplicate_bone(bone, new_name, size):
+    new_bone = amt.edit_bones.new(new_name + bone.name)
+    new_bone.head = bone.head
+    new_bone.tail = bone.head + (bone.tail - bone.head) * size
+    new_bone.roll = bone.roll
+    
+    #bpy.context.object.pose.bones[new_bone.name].custom_shape = None
+
+    return new_bone
+
 def select_layer(bone, l):
     bone.layers[l] = True
     for i in range(len(loc_bone.layers)):
@@ -17,49 +27,42 @@ if obj.type == 'ARMATURE':
     run_script = False
     amt = bpy.context.object.data
     selected_bones = []
-    for bone in amt.bones:
+    for bone in amt.edit_bones:
         if bone.select == True:
             run_script = True
-            selected_bones.append(bone)
+            selected_bones.append(bone.name)
+            print(bone, 'selected')
+    
     
     if run_script == True:
-        #bpy.ops.object.mode_set(mode='EDIT') 
-        for bone in selected_bones:
-            
-            bpy.ops.armature.select_all(action='DESELECT')
-            amt.edit_bones[bone.name].select = True
-            amt.edit_bones[bone.name].select_head = True
-            amt.edit_bones[bone.name].select_tail = True
-            
+         
+       
+        for i, bone in enumerate(selected_bones):
+            bpy.ops.object.mode_set(mode = 'EDIT') 
             #create location bone
-            bpy.ops.armature.duplicate()
-            bpy.ops.transform.resize(value=(0.5, 0.5, 0.5), constraint_axis=(False, False, False), constraint_orientation='NORMAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-            loc_bone = bpy.context.selected_bones[0]
-            loc_bone.name = ('MCH-loc-'+ bone.name)
-            loc_bone.head = bone.head_local
-            obj.pose.bones[loc_bone.name].custom_shape = None
+            loc_bone = duplicate_bone(amt.edit_bones[bone], 'MCH-loc-', 0.6)
             amt.layers[31] = True
             select_layer(loc_bone, 31)
             
             #create parent bone
-            bpy.ops.armature.duplicate()
-            bpy.ops.transform.resize(value=(0.5, 0.5, 0.5), constraint_axis=(False, False, False), constraint_orientation='NORMAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-            parent_bone = bpy.context.selected_editable_bones[0]
-            parent_bone.name = ('MCH-parent-'+ bone.name)
-            parent_bone.head = bone.head_local
-            obj.pose.bones[parent_bone.name].custom_shape = None
+            parent_bone = duplicate_bone(amt.edit_bones[bone], 'MCH-parent-', 0.3)
             amt.layers[30] = True
             select_layer(parent_bone, 30)
             
-            amt.edit_bones[bone.name].parent = amt.edit_bones[loc_bone.name]
-            amt.edit_bones[loc_bone.name].parent = amt.edit_bones['Master']
+            #create constraints
+            parent_bone.parent = amt.edit_bones[bone].parent
+            amt.edit_bones[bone].parent = loc_bone
+            loc_bone.parent = amt.edit_bones['Master']
+            loc_name = loc_bone.name
+            parent_name = parent_bone.name
+            
             bpy.ops.object.mode_set(mode='POSE') 
-            loc = obj.pose.bones[loc_bone.name].constraints.new('COPY_LOCATION')
+            loc = obj.pose.bones[loc_name].constraints.new('COPY_LOCATION')
             loc.target = obj
-            loc.subtarget = parent_bone.name
-            bpy.ops.object.mode_set(mode = 'EDIT') 
+            loc.subtarget = parent_name
+            #bpy.ops.object.mode_set(mode = 'EDIT')
             
     else:
         print('no bones')    
-    bpy.ops.object.mode_set(mode = current_mode) 
+    bpy.ops.object.mode_set(mode = current_mode)
     
